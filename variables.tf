5448efd6f200d796d0c367cacc1f685d618da810
Mukesh-97 <awsmukeshr@gmail.com>
2022-11-03 12:50:56 +0000

    use the self variable

--- variables.tf
+++ variables.tf
@@ -13,3 +13,20 @@ description = "value of Name for instance"
 type = string
 default = "Instance-1"
 }
+variable "instance_2_ami" {
+  description = "Value of the AMI ID for the EC2 instance"
+  type        = string
+  default     = "ami-08c40ec9ead489470"
+}
+
+variable "instance_2_type" {
+  description = "Value of the Instance Type for the EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_2_name" {
+  description = "Value of the Name Tag for the EC2 instance"
+  type        = string
+  default     = "instance-2"
+}
